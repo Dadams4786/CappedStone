aed91a9ef582688567593bec581d9f5728d5c6ed
Dadams4786 <58703714+Dadams4786@users.noreply.github.com>
2020-03-20 15:10:12 -0700

    Got things working mostly

--- capstone/fudr/views.py
+++ capstone/fudr/views.py
@@ -15,22 +15,66 @@ def recipes(request):
     params = dict(request.GET)
     params['apiKey'] = spoon_key
     params['number'] = 3
-    # params['addRecipeInformation'] = True
+    params['addRecipeInformation'] = True
+    params['fillIngredients'] = True
+    params['sort'] = 'random'
     response = requests.get('https://api.spoonacular.com/recipes/complexSearch', params=params)
-    recipes = json.loads(response.text)
-    return JsonResponse({'data': recipes})
+    print(response)
+    spoon_recipes = json.loads(response.text)
+    # print(spoon_recipes)
+    spoon_recipes = spoon_recipes["results"]
+    
+    recipes=[]
+    for spoon_recipe in spoon_recipes: 
+        try:
+            instructions = []
+            ingredients = []
+            for step in spoon_recipe["analyzedInstructions"][0]["steps"]:
+                instructions.append(step["step"])
+            for spoon_ingredient in spoon_recipe["missedIngredients"]:
+                ingredients.append(spoon_ingredient["original"])
+            ingredients = list(set(ingredients))
+            ingredients.sort()
+            recipes.append({
+                "title" : spoon_recipe["title"],
+                "image" : spoon_recipe["image"],
+                "summary" : spoon_recipe["summary"],
+                "instructions" : instructions,
+                "ingredients" : ingredients    
+            })
+        except IndexError:
+            continue
+
+    return JsonResponse({"recipes" : recipes})
 
 def randomRecipe(request):
     params = dict(request.GET)
     params['apiKey'] = spoon_key
     params['number'] = 3
     response = requests.get('https://api.spoonacular.com/recipes/random', params=params)
-    randRecipe = json.loads(response.text)
-    return JsonResponse({'data': randRecipe})
+    random_spoon_recipes = json.loads(response.text)
+    random_spoon_recipes = random_spoon_recipes["recipes"]
+    # print(random_spoon_recipes)
+    recipes=[]
+    for random_spoon_recipe in random_spoon_recipes:
+        try: 
+            instructions = []
+            ingredients = []
+            for step in random_spoon_recipe["analyzedInstructions"][0]["steps"]:
+                instructions.append(step["step"])
+            for ingredient in random_spoon_recipe["extendedIngredients"]:
+                ingredients.append(ingredient["original"])
+            ingredients = list(set(ingredients))
+            ingredients.sort()
+            recipes.append({
+                "title" : random_spoon_recipe["title"],
+                "image" : random_spoon_recipe["image"],
+                "summary" : random_spoon_recipe["summary"],
+                "instructions" : instructions,
+                "ingredients" : ingredients    
+            })
+        except IndexError:
+            continue
 
-# def cards(request):
-#     params = dict(request.GET)
-#     params['apiKey'] = spoon_key
-#     response = requests.get('https://api.spoonacular.com/recipes/visualizeRecipe', params=params)
-#     recipe_cards = json.loads(response)
-#     return JsonResponse(recipe_cards)
+    return JsonResponse({"recipes" : recipes})
+
